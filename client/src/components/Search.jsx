4882c48f2dc145e68c88e7534277ca436dd96ed1
Khai Tran <kqtran917@yahoo.com>
2021-10-22 15:59:36 -0500

    Implemented fix to search method

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -8,12 +8,14 @@ class Search extends React.Component {
     }
 
     this.onChange = this.onChange.bind(this)
+    this.search = this.search.bind(this)
   }
 
   onChange (e) {
     this.setState({
       term: e.target.value
     });
+    console.log(e.target.value)
   }
 
   search() {
@@ -22,6 +24,7 @@ class Search extends React.Component {
 
   render() {
     return (<div>
+      {console.log(this.props)}
       <h4>Add more repos!</h4>
       Enter a github username: <input value={this.state.terms} onChange={this.onChange}/>
       <button onClick={this.search}> Add Repos </button>
