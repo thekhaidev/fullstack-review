eb1c0e2f4b62e9689dcab48eff7578f72f8027a9
Khai Tran <kqtran917@yahoo.com>
2021-10-23 18:04:35 -0500

    Implemented dynamic list from state

--- client/src/components/ListItem.jsx
+++ client/src/components/ListItem.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
 
-
+const ListItem = (props) => (
+  <li>
+    <p>
+      Name: {props.name}
+      <br />
+      URL: {props.url}
+      <br />
+      WATCHES: {props.watcher}
+    </p>
+  </li>
+)
 
 
 export default ListItem
--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import ListItem from './ListItem.jsx'
 
 const RepoList = (props) => (
   <div>
     <h4> Repo List Component </h4>
     There are {props.repos.length} repos.
+    <ul>
+      {props.repos.map(repo => {
+        return <ListItem key={repo.url} name = {repo.name} url={repo.url} watchers={repo.watchers}/>})}
+    </ul>
   </div>
 )
 
