f827aa5cc24df2bc46cf1169bad671c46a00a1a0
Khai Tran <kqtran917@yahoo.com>
2021-10-23 12:29:11 -0500

    Implemented save first attempt

--- database/index.js
+++ database/index.js
@@ -14,6 +14,18 @@ let save = (repo) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+
+  let newRepo = new Repo({
+  	name: repo.name,
+  	url: repo.url
+  }).save((err, data) => {
+  	if (err) {
+  		console.log('Gonna be a no from me dawg: ', err);
+  	} else {
+  		console.log('Yay you saved that thang.');
+  	}
+  });
+
 }
 
 module.exports.save = save;
